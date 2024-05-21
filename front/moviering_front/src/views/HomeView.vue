efff518f3a1239a9a5a8c6e58dfc1e656cbcbc58
jjhoneyy <jjhoneyyyy@gmail.com>
2024-05-21 09:23:47 +0900

    feat) front 홈화면 movie list filtering 구현

--- front/moviering_front/src/views/HomeView.vue
+++ front/moviering_front/src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import data from '@/fixtures/movies.json'
+import data from '@/fixtures/movies2.json'
 </script>
 
 <template>
   <div class="container">
     <div class="main_row1">
-      Today's Movie
+      <p class="main_subtitle">Today's Movie</p>
       <img src="../img/movie4.jpeg" alt="home_movie_image" />
     </div>
     <span style="margin: 30px 0px;">---</span>
@@ -16,12 +16,38 @@ import data from '@/fixtures/movies.json'
     <RouterLink :to="{ path: '/movies' }">Movies</RouterLink>
     <RouterView />
     <span style="margin: 30px 0px;">---</span>
+
+    <p class="main_subtitle">Movie List(all)</p>
     <div class="movie_list_all" >
       <div v-for="movie in data">
-        <!-- {{ movie.fields.poster_path }} -->
+        <img class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
+      </div>
+    </div>
+    <span style="margin: 30px 0px;">---</span>
+
+    <p class="main_subtitle">Movie List(average > 7.5)</p>
+    <div class="movie_list_all" >
+      <div v-for="movie in data">
         <img v-if="movie.fields.vote_average > 7.5" class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
       </div>
     </div>
+    <span style="margin: 30px 0px;">---</span>
+
+    <p class="main_subtitle">Movie List(release date > 2000-00-00 )</p>
+    <div class="movie_list_all" >
+      <div v-for="movie in data">
+        <img v-if="movie.fields.release_date > '2000-00-00'" class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
+      </div>
+    </div>
+    <span style="margin: 30px 0px;">---</span>
+
+    <p class="main_subtitle">Movie List(music)</p>
+    <div class="movie_list_all" >
+      <div v-for="movie in data">
+        <img v-if="movie.fields.genre_ids.includes(10402)" class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -30,11 +56,15 @@ import data from '@/fixtures/movies.json'
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 100px;
 }
 .main_row1{
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.main_subtitle{
+  font-size: 20px;
 }
 h1 {
   color: rgb(83, 123, 255);
