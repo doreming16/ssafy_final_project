58648c914cdcd66824d0b965d833493ec14bd840
jjhoneyy <jjhoneyyyy@gmail.com>
2024-05-23 17:50:32 +0900

    feat) movielist filter 1차 작업 완 _ 2차 진행중

--- front/moviering_front/src/components/MovieRecommend.vue
+++ front/moviering_front/src/components/MovieRecommend.vue
@@ -2,10 +2,13 @@
     <div class="container">
       <h1>영화 추천</h1>
         
-      {{ userinfo }}
+      <!-- {{ userinfo }} -->
+      <!-- {{ props.userinfo }} -->
+        <p>myGender : {{ myGender }}</p>
 
         <div class="container">
             <h2 class="main_subtitle">장르별 추천</h2>
+            <p>myGenre: {{ myGenre }}</p>
             <div class="movie_list_all" >
                 <div v-for="movie in data">
                     <RouterLink :to="{ name: 'movie_detail_page', params: { id : movie.pk }}">
@@ -19,13 +22,12 @@
 
         <div class="container">
             <h2 class="main_subtitle">연도별 추천</h2>
-            <p>{{ props.userinfo[0].era }}</p>
-            <!-- 이거 함수 처리 -->
+            <p>myYear: {{ myYear }}</p>
 
             <div class="movie_list_all" >
-                <div v-for="movie in data">
+                <div v-for="movie in final_list">
                     <RouterLink :to="{ name: 'movie_detail_page', params: { id : movie.pk }}">
-                        <img class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
+                        <!-- <img class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image"> -->
                     </RouterLink>
                 </div>
             </div>
@@ -44,7 +46,14 @@
     
     const movieData = data
 
-    const movielist_year = ref([]);
+    // 연도별 filter
+    const movielist_filtered = ref([]);
+
+    // 새로 로그인해서 들어올 때 userinfo를 바로 못찾는건지 에러가 남 !
+    // 한 번 주석처리했다가 다시 풀면 작동됨 --- async 문젠가 ?
+    // props.userinfo[0].blahblah    이런 류를 주석처리하면 에러 안남
+    // promise에서 저 blahblah를 찾을 수 없다
+    // const myYear = props.userinfo[0].era
 
     const era_list = [
         {'title': 'bf1970','smaller': 1970, 'larger': 1970},
@@ -63,13 +72,31 @@
             const new_year = parseInt(release_year, 10)
 
             if (new_year >= 1970 && new_year < 1980){
-                movielist_year.value.push(toString(new_year))
+                movielist_filtered.value.push(toString(new_year))
             }
         }
     }
 
     filter_year(movieData)
+    console.log(movielist_filtered) // movielist_filter 1차 완성
 
+
+    // 장르별 filter
+    // const movielist_genre = ref([]);
+    // const myGenre = props.userinfo[0].favorite_genre
+    const filter_genre = function (data) {
+        
+    }
+
+
+    // 성별 filter
+    // const myGender = props.userinfo[0].gender
+    const filter_gender = function (data) {
+
+    }
+
+    const final_list = movielist_filtered.value
+    console.log(final_list)
 
   </script>
   
@@ -83,10 +110,10 @@
     align-items: center;
     padding-bottom: 100px;
     width: 100%;
-  }
-  .movie_list_all{
-    display: flex;
-    width: 100%;
-  }
+    }
+    .movie_list_all{
+        display: flex;
+        width: 100%;
+    }
   </style>
   
