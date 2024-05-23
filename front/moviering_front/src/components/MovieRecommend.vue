645a9fbfb2eaaff28abfdbf29dcb0c5df339eb40
doreming16 <jjhoneyyyy@gmail.com>
2024-05-23 22:07:27 +0900

    feat) front 연도별 추천 필터링 완료

--- front/moviering_front/src/components/MovieRecommend.vue
+++ front/moviering_front/src/components/MovieRecommend.vue
@@ -1,119 +1,124 @@
 <template>
+  <div class="container">
+    <h1>영화 추천</h1>
+
+    <!-- {{ userinfo }} -->
+    <!-- {{ props.userinfo }} -->
+    <p>myGender : {{ myGender }}</p>
+
     <div class="container">
-      <h1>영화 추천</h1>
-        
-      <!-- {{ userinfo }} -->
-      <!-- {{ props.userinfo }} -->
-        <p>myGender : {{ myGender }}</p>
-
-        <div class="container">
-            <h2 class="main_subtitle">장르별 추천</h2>
-            <p>myGenre: {{ myGenre }}</p>
-            <div class="movie_list_all" >
-                <div v-for="movie in data">
-                    <RouterLink :to="{ name: 'movie_detail_page', params: { id : movie.pk }}">
-                        <img class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image">
-                    </RouterLink>
-                </div>
-            </div>
+      <h2 class="main_subtitle">장르별 추천</h2>
+      <p>myGenre: {{ myGenre }}</p>
+      <div class="movie_list_all">
+        <div v-for="movie in data">
+          <RouterLink
+            :to="{ name: 'movie_detail_page', params: { id: movie.pk } }"
+          >
+            <img
+              class="poster_main"
+              :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`"
+              alt="Image"
+            />
+          </RouterLink>
         </div>
-
-      <span style="margin: 30px 0px;">---</span>
-
-        <div class="container">
-            <h2 class="main_subtitle">연도별 추천</h2>
-            <p>myYear: {{ myYear }}</p>
-
-            <div class="movie_list_all" >
-                <div v-for="movie in final_list">
-                    <RouterLink :to="{ name: 'movie_detail_page', params: { id : movie.pk }}">
-                        <!-- <img class="poster_main" :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`" alt="Image"> -->
-                    </RouterLink>
-                </div>
-            </div>
-        </div>
-
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-    import data from '@/fixtures/movies2.json'
-    import { ref } from 'vue'
 
-    const props = defineProps({
-        userinfo: Array
-    })
-    
-    const movieData = data
+    <span style="margin: 30px 0px">---</span>
 
-    // 연도별 filter
-    const movielist_filtered = ref([]);
+    <div class="container">
+      <h2 class="main_subtitle">연도별 추천</h2>
+      <p>myYear: {{ myYear }}</p>
 
-    // 새로 로그인해서 들어올 때 userinfo를 바로 못찾는건지 에러가 남 !
-    // 한 번 주석처리했다가 다시 풀면 작동됨 --- async 문젠가 ?
-    // props.userinfo[0].blahblah    이런 류를 주석처리하면 에러 안남
-    // promise에서 저 blahblah를 찾을 수 없다
-    // const myYear = props.userinfo[0].era
+      <div class="movie_list_all">
+        <div v-for="movie in final_list">
+          <RouterLink
+            :to="{ name: 'movie_detail_page', params: { id: movie.pk } }"
+          >
+            <img
+              class="poster_main"
+              :src="`https://image.tmdb.org/t/p/w300/${movie.fields.poster_path}`"
+              alt="Image"
+            />
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-    const era_list = [
-        {'title': 'bf1970','smaller': 1970, 'larger': 1970},
-        {'title': '1970s', 'smaller': 1970, 'larger': 1980},
-        {'title': '1980s', 'smaller': 1970, 'larger': 1980},
-        {'title': '1990s', 'smaller': 1970, 'larger': 1980},
-        {'title': '2000s', 'smaller': 1970, 'larger': 1980},
-        {'title': '2010s', 'smaller': 1970, 'larger': 1980},
-        {'title': 'af2020', 'smaller': 1970, 'larger': 1980}
-    ]
+<script setup>
+import data from "@/fixtures/movies2.json";
+import { ref } from "vue";
 
-    const filter_year = function (data) {
-        for (const movie of data){
-            const release_year = movie.fields.release_date.slice(0, 4)
+const props = defineProps({
+  userinfo: Array,
+});
 
-            const new_year = parseInt(release_year, 10)
+const movieData = data;
 
-            if (new_year >= 1970 && new_year < 1980){
-                movielist_filtered.value.push(movie)
-            }
-        }
+// 연도별 filter
+const movielist_filtered = ref([]);
+
+// 새로 로그인해서 들어올 때 userinfo를 바로 못찾는건지 에러가 남 !
+// 한 번 주석처리했다가 다시 풀면 작동됨 --- async 문젠가 ?
+// props.userinfo[0].blahblah    이런 류를 주석처리하면 에러 안남
+// promise에서 저 blahblah를 찾을 수 없다
+const myYear = props.userinfo[0].era;
+
+const filter_year = function (data) {
+  for (const movie of data) {
+    const release_year = movie.fields.release_date.slice(0, 4);
+
+    const new_year = parseInt(release_year, 10);
+
+    if (myYear === "bf1970" && new_year < 1970) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "1970s" && new_year < 1980) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "1980s" && new_year < 1990) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "1990s" && new_year < 2000) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "2000s" && new_year < 2010) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "2010s" && new_year < 2020) {
+      movielist_filtered.value.push(movie);
+    } else if (myYear === "af2020" && new_year >= 2020) {
+      movielist_filtered.value.push(movie);
     }
+  }
+};
 
-    filter_year(movieData)
-    console.log(movielist_filtered) // movielist_filter 1차 완성
+filter_year(movieData);
+console.log(movielist_filtered); // movielist_filter 1차 완성
 
+// 장르별 filter
+// const movielist_genre = ref([]);
+const myGenre = props.userinfo[0].favorite_genre;
+const filter_genre = function (data) {};
 
-    // 장르별 filter
-    // const movielist_genre = ref([]);
-    // const myGenre = props.userinfo[0].favorite_genre
-    const filter_genre = function (data) {
-        
-    }
+// 성별 filter
+const myGender = props.userinfo[0].gender;
+const filter_gender = function (data) {};
 
+const final_list = movielist_filtered.value;
+console.log(final_list);
+</script>
 
-    // 성별 filter
-    // const myGender = props.userinfo[0].gender
-    const filter_gender = function (data) {
-
-    }
-
-    const final_list = movielist_filtered.value
-    console.log(final_list)
-
-  </script>
-  
-  <style scoped>
-    *{
-        text-align: center;
-    }
-    .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 100px;
-    width: 100%;
-    }
-    .movie_list_all{
-        display: flex;
-        width: 100%;
-    }
-  </style>
-  
+<style scoped>
+* {
+  text-align: center;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 100px;
+  width: 100%;
+}
+.movie_list_all {
+  display: flex;
+  width: 100%;
+}
+</style>
